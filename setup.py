47f1c86cedf6665067068d55136c578a0514daaa
jo.brunet73@gmail.com <jo.brunet73@gmail.com>
2021-04-23 21:19:13 +0200

    package stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
-from setuptools import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
 setup(name='funniest',
       version='1.0.0',
-      description='The funniest joke in the world',
+      description='Software controlling the tension-inflation device',
+      long_description=long_description,  # Optional
+      long_description_content_type='text/markdown',
       url='https://github.com/JosephBrunet/Tension-inflation_GI.git',
       author='Joseph Brunet',
       author_email='jo.brunet73@gmail.com',
       license='MIT',
-      packages=['package'],
-      zip_safe=False)
+      packages=find_packages(where='package'), 
+      python_requires='>=3.5, <4',
+      install_requires=['pyqt','pyserial','pyqtgraph','simple-pid'],
+      entry_points={  # Optional
+        'console_scripts': [
+            'tension-inflation=entry_point:main',
+        ],
+      },
+      )
